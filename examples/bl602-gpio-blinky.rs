e6601fc517245822898085ca57b138425dff10b4
9names <60134748+9names@users.noreply.github.com>
2020-12-11 00:38:11 +1100

    Call clock init code and switch to using the delay function. Add work-around for clocks not being set correctly

--- examples/bl602-gpio-blinky.rs
+++ examples/bl602-gpio-blinky.rs
@@ -4,17 +4,23 @@
 use bl602_hal::{pac, prelude::*};
 
 use panic_halt as _;
-
+use bl602_hal::delay::McycleDelay;
 #[riscv_rt::entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
+    let mut dp = pac::Peripherals::take().unwrap();
+    bl602_hal::clock::glb_set_system_clk(&mut dp);
     let parts = dp.GLB.split();
     let mut gpio5 = parts.pin5.into_pull_down_output();
     gpio5.try_set_high().unwrap();
+    
+    // We should be running at 160MHz, but the glb_set_system_clk code isn't working correctly
+    // Set the sysclock reg to the freq we are running at
+    dp.HBN.hbn_rsv2.write(|w| unsafe {w.bits(32_000_000)});
+    let current_freq = dp.HBN.hbn_rsv2.read().bits();
+
+    let mut d = bl602_hal::delay::McycleDelay::new(current_freq);
     loop {
-        use riscv::register::mcycle;
-        let t0 = mcycle::read64();
-        while mcycle::read64().wrapping_sub(t0) <= 50_000_000 { }
+        d.try_delay_ms(1000);
         gpio5.try_toggle().unwrap();
     }
 }
